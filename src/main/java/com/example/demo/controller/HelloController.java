73b84ce98d21e1bc3035e6b9bc7b45b22b299fdf
bala-amypo <bala@amypo.in>
2025-12-21 16:38:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Rajapriya, Your Spring App Works 🎉";
+    }
+}
