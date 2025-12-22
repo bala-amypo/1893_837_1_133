acdab1090b0a8541809ad9e9951aa28d2db75c59
bala-amypo <bala@amypo.in>
2025-12-22 03:22:19 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class SwaggerConfig {
 
     @Bean
-    public Swagger customOpenAPI() {
+    public Swagger customSwagger() {
         return new Swagger()
                 // You need to change the port as per your server
                 .servers(List.of(
