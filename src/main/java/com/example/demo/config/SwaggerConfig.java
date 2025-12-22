fc5fe411466c7c2cd4caff3f11282a48b31a2b57
bala-amypo <bala@amypo.in>
2025-12-22 03:19:15 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,18 +1,20 @@
 package com.example.demo.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI api() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Inventory Balancer API")
-                        .description("API Documentation")
-                        .version("1.0.0"));
-    }
+    public Swagger customOpenAPI() {
+        return new Swagger()
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9370.pro604cr.amypo.ai/")
+                ));
+        }
 }
