54efa8f49fa4c80cd617189df77ef2287b665dd6
bala-amypo <bala@amypo.in>
2025-12-26 05:10:48 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,21 +1,19 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.GroupedOpenApi;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI apiInfo() {
-        return new OpenAPI().info(new Info().title("Multi-Location Inventory Balancer API").version("v1"));
-    }
-
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder().group("public").pathsToMatch("/**").build();
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Multi-Location Inventory Balancer API")
+                        .version("v1")
+                        .description("API documentation"));
     }
 }
