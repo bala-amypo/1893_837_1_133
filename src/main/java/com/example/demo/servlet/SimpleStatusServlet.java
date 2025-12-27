60117e78698ba10239b0fa47220ce67ad7e536a2
bala-amypo <bala@amypo.in>
2025-12-27 04:33:34 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/simple-status")
 public class SimpleStatusServlet extends HttpServlet {
     
-    // CHANGE: This must be PUBLIC so the test case can call it directly
+    // <--- CHANGED TO PUBLIC
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setStatus(HttpServletResponse.SC_OK);
